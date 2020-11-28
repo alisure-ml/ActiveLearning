52ff7c89769aec22c00f3331e73df2b0c3f07854
ALISURE <562282219@qq.com>
2020-11-28 21:29:10 +0800

    ufsl pn

--- UFSLviaIC/PN/pn_miniimagenet_two_ic_ufsl_2net_res_sgd_acc_duli.py
+++ UFSLviaIC/PN/pn_miniimagenet_two_ic_ufsl_2net_res_sgd_acc_duli.py
@@ -27,7 +27,6 @@ class MiniImageNetDataset(object):
         self.data_id = np.asarray(range(len(self.data_list)))
 
         self.classes = None
-        self.features = None
 
         self.data_dict = {}
         for index, label, image_filename in self.data_list:
@@ -55,10 +54,6 @@ class MiniImageNetDataset(object):
         self.classes = classes
         pass
 
-    def set_samples_feature(self, features):
-        self.features = features
-        pass
-
     def __getitem__(self, item):
         # 当前样本
         now_label_image_tuple = self.data_list[item]
@@ -66,7 +61,6 @@ class MiniImageNetDataset(object):
         _now_label = self.classes[item]
 
         now_label_k_shot_index = self._get_samples_by_clustering_label(_now_label, True, num=self.num_shot)
-        # now_label_k_shot_index = self._get_samples_by_clustering_label(_now_label, True, num=self.num_shot, now_index=now_index)
 
         is_ok_list = [self.data_list[one][1] == now_label_image_tuple[1] for one in now_label_k_shot_index]
 
@@ -94,29 +88,8 @@ class MiniImageNetDataset(object):
         task_index = torch.Tensor([one[0] for one in task_list]).long()
         return task_data, task_label, task_index, is_ok_list
 
-    def _get_samples_by_clustering_label(self, label, is_same_label=False, num=1, now_index=None, k=1):
+    def _get_samples_by_clustering_label(self, label, is_same_label=False, num=1):
         if is_same_label:
-            if now_index:
-                now_feature = self.features[now_index]
-
-                if k == 1:
-                    search_index = self.data_id[self.classes == label]
-                else:
-                    top_k_class = np.argpartition(now_feature, -k)[-k:]
-                    search_index = np.hstack([self.data_id[self.classes == one] for one in top_k_class])
-                    pass
-
-                search_index_list = list(search_index)
-                if now_index in search_index_list:
-                    search_index_list.remove(now_index)
-                other_features = self.features[search_index_list]
-
-                # sim_result = np.matmul(other_features, now_feature)
-                now_features = np.tile(now_feature[None, ...], reps=[other_features.shape[0], 1])
-                sim_result = np.sum(now_features * other_features, axis=-1)
-
-                sort_result = np.argsort(sim_result)[::-1]
-                return list(search_index[sort_result][0: num])
             return random.sample(list(np.squeeze(np.argwhere(self.classes == label), axis=1)), num)
         else:
             return random.sample(list(np.squeeze(np.argwhere(self.classes != label))), num)
@@ -182,7 +155,6 @@ class ProduceClass(object):
         self.count_2 = 0
         self.class_num = np.zeros(shape=(self.out_dim,), dtype=np.int)
         self.classes = np.zeros(shape=(self.n_sample,), dtype=np.int)
-        self.features = np.random.random(size=(self.n_sample, self.out_dim))
         pass
 
     def init(self):
@@ -204,8 +176,6 @@ class ProduceClass(object):
         out_data = out.data.cpu()
         top_k = out_data.topk(self.out_dim, dim=1)[1].cpu()
         indexes_cpu = indexes.cpu()
-
-        self.features[indexes_cpu] = out_data
 
         batch_size = top_k.size(0)
         class_labels = np.zeros(shape=(batch_size,), dtype=np.int)
@@ -248,7 +218,6 @@ class Runner(object):
         self.produce_class = ProduceClass(len(self.data_train), Config.ic_out_dim, Config.ic_ratio)
         self.produce_class.init()
         self.task_train.set_samples_class(self.produce_class.classes)
-        self.task_train.set_samples_feature(self.produce_class.features)
 
         # model
         self.proto_net = RunnerTool.to_cuda(Config.proto_net)
@@ -442,7 +411,7 @@ class Runner(object):
 
 
 class Config(object):
-    gpu_id = 1
+    gpu_id = 0
     os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
 
     num_workers = 16
